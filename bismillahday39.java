a546c4043a3a9ca464ca771bf089be1efd669ef1
afvgn <isfanurmaisyarah@gmail.com>
2024-10-10 21:54:45 +0800

    Create bismillahday39.java

---
+++ bismillahday39.java
@@ -0,0 +1,11 @@
+public class SimpleOperator {
+    public static void main(String[] args) {
+        int a = 10;  
+        int b = 3;   
+        int sisa = a % b; 
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+        System.out.println("Sisa a % b: " + sisa);
+    }
+}
