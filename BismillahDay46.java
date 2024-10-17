89a1a753abda433d497016a1e6aa469345e1e829
afvgn <isfanurmaisyarah@gmail.com>
2024-10-17 23:16:45 +0800

    Create BismillahDay46.java

---
+++ BismillahDay46.java
@@ -0,0 +1,10 @@
+public class ORLogic {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        boolean result = a || b;
+
+        System.out.println("Hasil dari a || b: " + result);
+    }
+}
