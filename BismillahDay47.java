5585446f0051471526d813bed963f142bca72373
afvgn <isfanurmaisyarah@gmail.com>
2024-10-18 19:23:20 +0800

    Create BismillahDay47.java

---
+++ BismillahDay47.java
@@ -0,0 +1,12 @@
+public class OperatorLogikaNegasi {
+    public static void main(String[] args) {
+        boolean nilaiBenar = true;
+        boolean nilaiSalah = false;
+
+        System.out.println("Nilai asli: " + nilaiBenar);
+        System.out.println("Nilai setelah negasi: " + !nilaiBenar);
+
+        System.out.println("Nilai asli: " + nilaiSalah);
+        System.out.println("Nilai setelah negasi: " + !nilaiSalah);
+    }
+}
