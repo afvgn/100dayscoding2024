753fe17c1b33e41973725b5da2ae3b3e6d3115b7
afvgn <isfanurmaisyarah@gmail.com>
2024-10-19 20:22:51 +0800

    Create BismillahDay48.java

---
+++ BismillahDay48.java
@@ -0,0 +1,13 @@
+public class PercabanganIf {
+    public static void main(String[] args) {
+        int nilai = 75;
+
+        if (nilai >= 75) {
+            System.out.println("Lulus");
+        }
+
+        if (nilai < 75) {
+            System.out.println("Tidak Lulus");
+        }
+    }
+}
