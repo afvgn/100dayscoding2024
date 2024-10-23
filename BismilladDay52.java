06db33241692b365abd642b9717e2fc7ea98f04c
afvgn <isfanurmaisyarah@gmail.com>
2024-10-23 19:19:14 +0800

    Create BismilladDay52.java

---
+++ BismilladDay52.java
@@ -0,0 +1,12 @@
+
+public class contohTernary {
+    public static void main(String[] args) {
+        int nilai = 75;
+        String hasil;
+
+        hasil = (nilai >= 60) ? "Lulus" : "Tidak Lulus";
+
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Hasil: " + hasil);
+    }
+}
