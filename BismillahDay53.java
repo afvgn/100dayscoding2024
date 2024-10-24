a70390e5945a9fdf30d1a3718737256ffa36690a
afvgn <isfanurmaisyarah@gmail.com>
2024-10-24 20:26:03 +0800

    Create BismillahDay53.java

---
+++ BismillahDay53.java
@@ -0,0 +1,7 @@
+public class contohLooping {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Angka: " + i);
+        }
+    }
+}
