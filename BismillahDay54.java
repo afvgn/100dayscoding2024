2d1f90dcdec9e04a0d99c70ae69342bd087335a2
afvgn <isfanurmaisyarah@gmail.com>
2024-10-25 21:14:10 +0800

    Create BismillahDay54.java

---
+++ BismillahDay54.java
@@ -0,0 +1,7 @@
+public class LoopIncrement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Nilai i: " + i);
+        }
+    }
+}
