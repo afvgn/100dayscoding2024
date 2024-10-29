2799c4e111943e60b25c5ae0f144ba1698ad24d0
afvgn <isfanurmaisyarah@gmail.com>
2024-10-29 21:51:02 +0800

    Create BismillahDay58.java

---
+++ BismillahDay58.java
@@ -0,0 +1,12 @@
+public class contohWhileLoopDecrement {
+    public static void main(String[] args) {
+        int number = 10;
+        
+        while (number > 0) {
+            System.out.println("Angka saat ini: " + number);
+            number--;
+        }
+        
+        System.out.println("Loop selesai!");
+    }
+}
