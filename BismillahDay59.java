93d69388945862dced512cc88815b98d9613479e
afvgn <isfanurmaisyarah@gmail.com>
2024-10-30 21:38:40 +0800

    Create BismillahDay59.java

---
+++ BismillahDay59.java
@@ -0,0 +1,9 @@
+public class ContohEachExample {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+
+        for (int num : angka) {
+            System.out.println("Angka: " + num);
+        }
+    }
+}
