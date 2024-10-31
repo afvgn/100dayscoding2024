ecd7ec2315d24e9cfb20bc31846e6ccfc4bb53b2
afvgn <isfanurmaisyarah@gmail.com>
2024-10-31 22:46:17 +0800

    Create BismillahDay60.java

---
+++ BismillahDay60.java
@@ -0,0 +1,10 @@
+public class contohDoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while (i <= 5);
+    }
+}
